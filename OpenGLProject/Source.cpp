18449a9f899dfb3439d3a8e5258216b0ef79d812
chen <812668250@qq.com>
2019-05-14 16:38:25 +0800

    20190514

--- OpenGLProject/Source.cpp
+++ OpenGLProject/Source.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <glad/glad.h> 
 #include <GLFW\glfw3.h>
-///
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
@@ -13,6 +13,31 @@ void processInput(GLFWwindow *window)
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
 }
+const char *vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+const char *fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
+///顶点
+float vertices[] = {
+	0.5f, 0.5f, 0.0f,   // 右上角
+	0.5f, -0.5f, 0.0f,  // 右下角
+	-0.5f, -0.5f, 0.0f, // 左下角
+	-0.5f, 0.5f, 0.0f   // 左上角
+};
+///顶点索引
+unsigned int indices[] = { // 注意索引从0开始! 
+	0, 1, 3, // 第一个三角形
+	1, 2, 3  // 第二个三角形
+};
+
 int main()
 {
 	///初始化GLFW
@@ -40,6 +65,78 @@ int main()
 	glViewport(0, 0, 800, 600);
 	///注册一个回调函数处理窗口大小被调整时
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+#pragma region GLSetting
+	///创建VBO，VAO
+	unsigned int VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	///创建索引
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+	///绑定缓冲类型
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	///把顶点数据复制到缓冲内存中
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	///吧索引复制到缓冲区内存中
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	///创建顶点着色器
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	///编译顶点着色器
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	///获得顶点着色器编译状态
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	///创建片着色器
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	///编译片着色器
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	///获得片着色器编译状态
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	///将着色器连接成着色程序
+	///创建着色程序
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	///链接着色器
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	///获得链接状态
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAMLINK::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	///释放不需要的着色器
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+	///链接顶点属性
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	//请注意，这是允许的，对glVertexAttribPointer的调用将VBO注册为顶点属性的绑定顶点缓冲区对象，因此我们可以安全地解除绑定
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	//您可以在之后取消绑定VAO，以便其他VAO调用不会意外地修改此VAO，但这种情况很少发生。我们通常不会在不直接需要时取消绑定VAO（或者VBO）。
+	glBindVertexArray(0);
+#pragma endregion
 	///渲染循环（Render Loop）
 	while (!glfwWindowShouldClose(window))
 	{
@@ -47,12 +144,23 @@ int main()
 		///渲染指令
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+		///draw
+		///激活程序对象
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		///绘制顶点
+		///glDrawArrays(GL_TRIANGLES, 0, 3);
+		///按照索引绘制
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		///函数会交换颜色缓冲（它是一个储存着GLFW窗口每一个像素颜色值的大缓冲），它在这一迭代中被用来绘制，并且将会作为输出显示在屏幕上。
 		glfwSwapBuffers(window);
 		///函数检查有没有触发什么事件（比如键盘输入、鼠标移动等）、更新窗口状态，并调用对应的回调函数（可以通过回调方法手动设置）。
 		glfwPollEvents();
 	}
 	///清理资源退出
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	glfwTerminate();
 	return 0;
 }
